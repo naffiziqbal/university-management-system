653c6fa1a2fbddb73274aee0f548b149f15782e9
naffiziqbal <f.nafiziqbal@gmail.com>
2023-05-31 14:06:48 +0600

    Keep calm and commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,8 +6,8 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Online')
-    console.log('Sever Successfull')
+  res.send('Online')
+  console.log('Sever Successfull')
 })
 
 export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,6 @@ import path from 'path'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port: process.env.PORT,
-    database_url: process.env.DB_URL,
+  port: process.env.PORT,
+  database_url: process.env.DB_URL,
 }
