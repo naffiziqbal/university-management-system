233d125a6ab4daca0cab6c7dae2408d74a3b92b5
naffiziqbal <f.nafiziqbal@gmail.com>
2023-05-31 14:26:17 +0600

    Checking

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
-app.use(cors())
+                               app.use(cors())
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
