5ae8201601d255f467dde5b7905b606e912f6d5c
naffiziqbal <f.nafiziqbal@gmail.com>
2023-06-05 23:26:51 +0600

    Error Handlelling

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import router from './modules/users/users.router'
 import { successLogger } from '../share/logger'
@@ -8,7 +8,37 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/v1/', router)
 
-// console.log(app.get("env"))
+//** Custom Error Message  */
+
+class ApiError extends Error {
+  statusCode: number
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+//! Testing
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  throw new ApiError(404, 'Custom Error Message')
+  // next("Custom Err On Next Function")
+})
+
+//** Log  Production Level */
 successLogger.info(app.get('env'))
 
+//** Error Handler  */
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err })
+  } else {
+    res.status(500).json({ error: 'Something Went Wrong ' })
+  }
+})
 export default app
