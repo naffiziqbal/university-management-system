7dd80f466e735796f0a5cd1037615a6e550940d7
naffiziqbal <f.nafiziqbal@gmail.com>
2023-06-14 21:30:40 +0600

    Zod Handelling Final

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,30 @@
-import mongoose from 'mongoose'
-import app from './app/app'
-import config from './config'
-import { errorLogger, infoLogger, successLogger } from './share/logger'
+import mongoose from 'mongoose';
+import app from './app/app';
+import config from './config';
+import { errorLogger, infoLogger, successLogger } from './share/logger';
 // import ApiError from './erros/apiErrors'
-import { Server } from 'http' // Default Package of Node
+import { Server } from 'http'; // Default Package of Node
 
 //   Handle Uncaught Exeption
 process.on('uncaughtException', error => {
-  errorLogger.error(error.message)
-  process.exit(1)
-})
+  errorLogger.error(error.message);
+  process.exit(1);
+});
 
-const port = config.port
-let server: Server
+const port = config.port;
+let server: Server;
 // Database Connection Function
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string)
-    successLogger.info('Database Connection Successfull')
+    await mongoose.connect(config.database_url as string);
+    successLogger.info('Database Connection Successfull');
 
     server = app.listen(port, () => {
-      successLogger.info(`Application is Listening on Port ${port} `)
-    })
+      successLogger.info(`Application is Listening on Port ${port} `);
+    });
     //eslint-disable-next-line
   } catch (error: any) {
-    errorLogger.error('Failde To Connect Database', error)
+    errorLogger.error('Failde To Connect Database', error);
   }
 
   //  To Handle abrupt Server Crash
@@ -32,21 +32,21 @@ async function main() {
   process.on('unhandledRejection', err => {
     if (server) {
       //eslint-disable-next-line
-      console.log('Unhandle')
+      console.log('Unhandle');
       server.close(() => {
-        errorLogger.error(err)
-        process.exit(1)
-      })
+        errorLogger.error(err);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
-main()
+main();
 
 process.on('SIGTERM', () => {
-  infoLogger.info('SIGTERM Recived')
+  infoLogger.info('SIGTERM Recived');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
