829fdb4aa0068436f0485b2330ab2f1301bff803
naffiziqbal <f.nafiziqbal@gmail.com>
2023-06-16 22:35:21 +0600

    App Ts Edit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-import router from './modules/user/user.router';
+import { UserRoute } from './modules/user/user.router';
 import { successLogger } from '../share/logger';
 import ApiError from '../erros/apiErrors';
 import { globaErrorHandler } from '../middleware/globalErrorHandler';
+import { SemesterRoute } from './modules/accademicSemester/accademicSemester.route';
 
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/', router);
+app.use('/api/v1/', UserRoute);
+app.use('/api/v1/', SemesterRoute);
 
 //! Testing
 
